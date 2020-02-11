3a355987484fbeb41fb2c4cf73a616c66ca2373b
Sébastien Bouchard <31940713+SebastJava@users.noreply.github.com>
2020-02-11 15:59:11 -0500

    New Mint-Y colors

--- generate-themes.py
+++ generate-themes.py
@@ -96,57 +96,57 @@ for color in os.listdir("src/Mint-X/variations"):
 
 # Mint-Y
 Y_HEX_ACCENT1 = ["#92b372"]  # BASE
-Y_HEX_ACCENT2 = ["#8fa876"]
+Y_HEX_ACCENT2 = ["#8fa876"]  # BASE-DARK
 Y_HEX_ACCENT3 = ["#afca95"]  # PRELIGHT/HOVER
 Y_HEX_ACCENT4 = ["#779559"]  # PRESSED
 
 y_hex_colors1 = {}
-y_hex_colors1["Aqua"] = "#6cabcd"
-y_hex_colors1["Blue"] = "#5b73c4"
-y_hex_colors1["Brown"] = "#aa876a"
-y_hex_colors1["Grey"] = "#9d9d9d"
-y_hex_colors1["Orange"] = "#db9d61"
-y_hex_colors1["Pink"] = "#c76199"
-y_hex_colors1["Purple"] = "#8c6ec9"
-y_hex_colors1["Red"] = "#c15b58"
-y_hex_colors1["Sand"] = "#c8ac69"
-y_hex_colors1["Teal"] = "#5aaa9a"
+y_hex_colors1["Aqua"] = "#0BB2B4"
+y_hex_colors1["Blue"] = "#0F97D9"
+y_hex_colors1["Brown"] = "#7F542B"
+y_hex_colors1["Grey"] = "#767676"
+y_hex_colors1["Orange"] = "#EF6410"
+y_hex_colors1["Pink"] = "#E54980"
+y_hex_colors1["Purple"] = "#B41AB4"
+y_hex_colors1["Red"] = "#DF2121"
+y_hex_colors1["Sand"] = "#927D4E"
+y_hex_colors1["Teal"] = "#008080"
 
 y_hex_colors2 = {}
-y_hex_colors2["Aqua"] = "#6aa0bd"
-y_hex_colors2["Blue"] = "#596eb5"
-y_hex_colors2["Brown"] = "#9c7e65"
-y_hex_colors2["Grey"] = "#8f8f8f"
-y_hex_colors2["Orange"] = "#cc9560"
-y_hex_colors2["Pink"] = "#b85f90"
-y_hex_colors2["Purple"] = "#866cba"
-y_hex_colors2["Red"] = "#b35a57"
-y_hex_colors2["Sand"] = "#b89f65"
-y_hex_colors2["Teal"] = "#579c8e"
+y_hex_colors2["Aqua"] = "#0BA0A1"
+y_hex_colors2["Blue"] = "#0D89C3"
+y_hex_colors2["Brown"] = "#7F542B"
+y_hex_colors2["Grey"] = "#767676"
+y_hex_colors2["Orange"] = "#DF5807"
+y_hex_colors2["Pink"] = "#D93D74"
+y_hex_colors2["Purple"] = "#9E22A0"
+y_hex_colors2["Red"] = "#CA1E1E"
+y_hex_colors2["Sand"] = "#927D4E"
+y_hex_colors2["Teal"] = "#007474"
 
 y_hex_colors3 = {}
-y_hex_colors3["Aqua"] = "#82b3ce"
-y_hex_colors3["Blue"] = "#6e82c6"
-y_hex_colors3["Brown"] = "#ad9078"
-y_hex_colors3["Grey"] = "#a3a3a3"
-y_hex_colors3["Orange"] = "#dcaa7a"
-y_hex_colors3["Pink"] = "#c975a3"
-y_hex_colors3["Purple"] = "#9b84cb"
-y_hex_colors3["Red"] = "#d1716f"
-y_hex_colors3["Sand"] = "#c9b27c"
-y_hex_colors3["Teal"] = "#6dbbab"
+y_hex_colors3["Aqua"] = "#5DCCCD"
+y_hex_colors3["Blue"] = "#5FBAE6"
+y_hex_colors3["Brown"] = "#AA8D72"
+y_hex_colors3["Grey"] = "#A4A4A4"
+y_hex_colors3["Orange"] = "#F49860"
+y_hex_colors3["Pink"] = "#EE86AA"
+y_hex_colors3["Purple"] = "#CD66CD"
+y_hex_colors3["Red"] = "#EA6B6B"
+y_hex_colors3["Sand"] = "#B6A889"
+y_hex_colors3["Teal"] = "#55AAAA"
 
 y_hex_colors4 = {}
-y_hex_colors4["Aqua"] = "#52819a"
-y_hex_colors4["Blue"] = "#4b5a8d"
-y_hex_colors4["Brown"] = "#796555"
-y_hex_colors4["Grey"] = "#727272"
-y_hex_colors4["Orange"] = "#a97747"
-y_hex_colors4["Pink"] = "#924d73"
-y_hex_colors4["Purple"] = "#6a5497"
-y_hex_colors4["Red"] = "#9a4a47"
-y_hex_colors4["Sand"] = "#947f51"
-y_hex_colors4["Teal"] = "#4e8378"
+y_hex_colors4["Aqua"] = "#087778"
+y_hex_colors4["Blue"] = "#0A6591"
+y_hex_colors4["Brown"] = "#55381D"
+y_hex_colors4["Grey"] = "#4F4F4F"
+y_hex_colors4["Orange"] = "#9F430B"
+y_hex_colors4["Pink"] = "#993155"
+y_hex_colors4["Purple"] = "#781178"
+y_hex_colors4["Red"] = "#951616"
+y_hex_colors4["Sand"] = "#615334"
+y_hex_colors4["Teal"] = "#005555"
 
 curdir = os.getcwd()
 
@@ -250,4 +250,4 @@ for color in y_hex_colors1.keys():
 
 
 # Files
-os.system("cp -R files/* ./")
+# os.system("cp -R files/* ./")
