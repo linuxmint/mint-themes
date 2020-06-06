48858ca04b2650b1ab69d7f75e58dff064bb2be9
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2020-06-06 10:53:05 +0100

    Revert "Factorize color codes into a single constants.py file"
    This reverts commit b3fa5a261fd3f83c832582da46f5c2a642363639.

--- generate-themes.py
+++ generate-themes.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 import os
 
-from constants import X_HEX_ACCENTS, X_RGB_ACCENTS, x_hex_colors, x_rgb_colors
-from constants import Y_HEX_ACCENT1, Y_HEX_ACCENT2, Y_HEX_ACCENT3, Y_HEX_ACCENT4
-from constants import y_hex_colors1, y_hex_colors2, y_hex_colors3, y_hex_colors4
-
 def change_value (key, value, file):
     if value is not None:
         command = "sed -i '/%(key)s=/c\%(key)s=%(value)s' %(file)s" % {'key':key, 'value':value, 'file':file}
@@ -27,6 +23,35 @@ def y_colorize_directory (path, variation):
         os.system("find %s -name '*.*' -type f -exec sed -i 's/%s/%s/gI' {}  \\;" % (path, accent, y_hex_colors3[variation]))
     for accent in Y_HEX_ACCENT4:
         os.system("find %s -name '*.*' -type f -exec sed -i 's/%s/%s/gI' {}  \\;" % (path, accent, y_hex_colors4[variation]))
+
+# Mint-X
+X_HEX_ACCENTS = ["#9ab87c", "#accd8a"]
+
+x_hex_colors = {}
+x_hex_colors["Aqua"] = "#6cabcd"
+x_hex_colors["Blue"] = "#5b73c4"
+x_hex_colors["Brown"] = "#aa876a"
+x_hex_colors["Grey"] = "#9d9d9d"
+x_hex_colors["Orange"] = "#db9d61"
+x_hex_colors["Pink"] = "#c76199"
+x_hex_colors["Purple"] = "#8c6ec9"
+x_hex_colors["Red"] = "#c15b58"
+x_hex_colors["Sand"] = "#c8ac69"
+x_hex_colors["Teal"] = "#5aaa9a"
+
+X_RGB_ACCENTS = ["172, 205, 138", "#accd8a"]
+
+x_rgb_colors = {}
+x_rgb_colors["Aqua"] = "108, 171, 205"
+x_rgb_colors["Blue"] = "91, 115, 196"
+x_rgb_colors["Brown"] = "170, 135, 106"
+x_rgb_colors["Grey"] = "157,157,157"
+x_rgb_colors["Orange"] = "219, 157, 97"
+x_rgb_colors["Pink"] = "199, 97, 153"
+x_rgb_colors["Purple"] = "140, 110, 201"
+x_rgb_colors["Red"] = "193, 91, 88"
+x_rgb_colors["Sand"] = "200, 172, 105"
+x_rgb_colors["Teal"] = "90, 170, 154"
 
 if os.path.exists("usr"):
     os.system("rm -rf usr/")
@@ -67,6 +92,61 @@ for color in os.listdir("src/Mint-X/variations"):
                 os.system("sed -i s'/%(accent)s/%(color_accent)s/' %(file)s" % {'accent': accent, 'color_accent': x_hex_colors[color], 'file': file})
             for accent in X_RGB_ACCENTS:
                 os.system("sed -i s'/%(accent)s/%(color_accent)s/' %(file)s" % {'accent': accent, 'color_accent': x_rgb_colors[color], 'file': file})
+
+
+# Mint-Y
+Y_HEX_ACCENT1 = ["#92b372"]  # BASE
+Y_HEX_ACCENT2 = ["#8fa876"]
+Y_HEX_ACCENT3 = ["#afca95"]  # PRELIGHT/HOVER
+Y_HEX_ACCENT4 = ["#779559"]  # PRESSED
+
+y_hex_colors1 = {}
+y_hex_colors1["Aqua"] = "#6cabcd"
+y_hex_colors1["Blue"] = "#5b73c4"
+y_hex_colors1["Brown"] = "#aa876a"
+y_hex_colors1["Grey"] = "#9d9d9d"
+y_hex_colors1["Orange"] = "#db9d61"
+y_hex_colors1["Pink"] = "#e54980"
+y_hex_colors1["Purple"] = "#8c6ec9"
+y_hex_colors1["Red"] = "#c15b58"
+y_hex_colors1["Sand"] = "#c8ac69"
+y_hex_colors1["Teal"] = "#5aaa9a"
+
+y_hex_colors2 = {}
+y_hex_colors2["Aqua"] = "#6aa0bd"
+y_hex_colors2["Blue"] = "#596eb5"
+y_hex_colors2["Brown"] = "#9c7e65"
+y_hex_colors2["Grey"] = "#8f8f8f"
+y_hex_colors2["Orange"] = "#cc9560"
+y_hex_colors2["Pink"] = "#d93d74"
+y_hex_colors2["Purple"] = "#866cba"
+y_hex_colors2["Red"] = "#b35a57"
+y_hex_colors2["Sand"] = "#b89f65"
+y_hex_colors2["Teal"] = "#579c8e"
+
+y_hex_colors3 = {}
+y_hex_colors3["Aqua"] = "#82b3ce"
+y_hex_colors3["Blue"] = "#6e82c6"
+y_hex_colors3["Brown"] = "#ad9078"
+y_hex_colors3["Grey"] = "#a3a3a3"
+y_hex_colors3["Orange"] = "#dcaa7a"
+y_hex_colors3["Pink"] = "#ee86aa"
+y_hex_colors3["Purple"] = "#9b84cb"
+y_hex_colors3["Red"] = "#d1716f"
+y_hex_colors3["Sand"] = "#c9b27c"
+y_hex_colors3["Teal"] = "#6dbbab"
+
+y_hex_colors4 = {}
+y_hex_colors4["Aqua"] = "#52819a"
+y_hex_colors4["Blue"] = "#4b5a8d"
+y_hex_colors4["Brown"] = "#796555"
+y_hex_colors4["Grey"] = "#727272"
+y_hex_colors4["Orange"] = "#a97747"
+y_hex_colors4["Pink"] = "#993155"
+y_hex_colors4["Purple"] = "#6a5497"
+y_hex_colors4["Red"] = "#9a4a47"
+y_hex_colors4["Sand"] = "#947f51"
+y_hex_colors4["Teal"] = "#4e8378"
 
 curdir = os.getcwd()
 
