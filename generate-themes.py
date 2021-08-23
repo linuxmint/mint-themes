19b3e19bb04f6ee9b083d799b2ad5a7e9760e889
JosephMcc <mccullarjoseph@gmail.com>
2021-08-23 13:39:47 -0700

    Mint-Y: Only ship a single metacity theme
    We know longer need the -dark variation. We also no longer need to support
    the metacity-2 format since all window manager we use support v3.

--- generate-themes.py
+++ generate-themes.py
@@ -94,9 +94,6 @@ for color in y_hex_colors1.keys():
             for key in ["IconTheme"]:
                 change_value(key, "%s-%s" % (original_name, color), theme_index)
 
-            for key in ["MetacityTheme"]:
-                change_value(key, "%s" % original_name, theme_index)
-
             # Regenerate GTK3 sass
             os.system("cp -R src/Mint-Y/gtk-3.0/sass %s/gtk-3.0/" % theme)
             y_colorize_directory("%s/gtk-3.0/sass" % theme, color)
@@ -128,8 +125,6 @@ for color in y_hex_colors1.keys():
             files.append(os.path.join(theme, "gtk-2.0", "panel.rc"))
             files.append(os.path.join(theme, "gtk-2.0", "apps.rc"))
             files.append(os.path.join(theme, "gtk-2.0", "menubar-toolbar", "gtkrc"))
-            files.append(os.path.join(theme, "metacity-1", "metacity-theme-2.xml"))
-            files.append(os.path.join(theme, "metacity-1", "metacity-theme-3.xml"))
             for file in files:
                 if os.path.exists(file):
                     for accent in Y_HEX_ACCENT1:
--- src/Mint-Y/build-themes.py
+++ src/Mint-Y/build-themes.py
@@ -58,7 +58,6 @@ if __name__ == '__main__':
             os.system("cp gtk-3.0/thumbnail.png %s" % version_folder)
             # Metacity
             os.system("cp -R metacity-1 %s" % dest_folder)
-            os.system("rm %s/*-dark*" % (os.path.join(dest_folder, "metacity-1")))
             # Cinnamon
             version_folder = os.path.join(dest_folder, "cinnamon")
             os.system("mkdir -p %s" % version_folder)
@@ -87,11 +86,6 @@ if __name__ == '__main__':
             os.system("cp -R gtk-3.0/assets %s" % version_folder)
             os.system("cp gtk-3.0/gtk-dark.css %s" % os.path.join(version_folder, "gtk.css"))
             os.system("cp gtk-3.0/thumbnail-dark.png %s" % os.path.join(version_folder, "thumbnail.png"))
-            # Metacity
-            os.system("cp -R metacity-1 %s" % dest_folder)
-            os.system("mv %s %s" % (os.path.join(dest_folder, "metacity-1", "metacity-theme-2-dark.xml"), os.path.join(dest_folder, "metacity-1", "metacity-theme-2.xml")))
-            os.system("mv %s %s" % (os.path.join(dest_folder, "metacity-1", "metacity-theme-3-dark.xml"), os.path.join(dest_folder, "metacity-1", "metacity-theme-3.xml")))
-            os.system("mv %s %s" % (os.path.join(dest_folder, "metacity-1", "thumbnail-dark.png"), os.path.join(dest_folder, "metacity-1", "thumbnail.png")))
             # Cinnamon
             version_folder = os.path.join(dest_folder, "cinnamon")
             os.system("mkdir -p %s" % version_folder)
