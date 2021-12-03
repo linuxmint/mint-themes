c3dd76a6a3b5ab163ec1063e65e311addc5a197d
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2021-12-03 10:42:11 +0000

    Update Xfwm4 theme

--- src/Mint-Y/build-themes.py
+++ src/Mint-Y/build-themes.py
@@ -34,6 +34,18 @@ print("Cinnamon assets updated")
 
 os.chdir(curdir)
 
+print("Updating Xfwm4 assets")
+os.chdir("xfwm4/")
+os.system("./render-assets.sh")
+
+os.chdir(curdir)
+
+print("Updating Xfwm4 dark assets")
+os.chdir("xfwm4-dark/")
+os.system("./render-assets.sh")
+
+os.chdir(curdir)
+
 if __name__ == '__main__':
     print("Building themes")
     for variation in VARIATIONS:
@@ -65,7 +77,10 @@ if __name__ == '__main__':
             os.system("cp cinnamon/mint-y-thumbnail.png %s" % os.path.join(version_folder, "thumbnail.png"))
             os.system("cp cinnamon/cinnamon.css %s" % version_folder)
             # XFWM
-            os.system("cp -R xfwm4 %s" % dest_folder)
+            version_folder = os.path.join(dest_folder, "xfwm4")
+            os.system("mkdir -p %s" % version_folder)
+            os.system("cp -R xfwm4/*.png %s" % version_folder)
+            os.system("cp -R xfwm4/themerc %s" % version_folder)
 
         elif variation == "Mint-Y-Dark":
             print("    Building Mint-Y-Dark")
@@ -93,6 +108,7 @@ if __name__ == '__main__':
             os.system("cp cinnamon/mint-y-dark-thumbnail.png %s" % os.path.join(version_folder, "thumbnail.png"))
             os.system("cp cinnamon/cinnamon-dark.css %s" % os.path.join(version_folder, "cinnamon.css"))
             # XFWM
-            os.system("rm -rf %s" % os.path.join(dest_folder, "xfwm4"))
-            os.system("cp -R xfwm4-dark %s" % dest_folder)
-            os.system("mv %s %s" % (os.path.join(dest_folder, "xfwm4-dark"), os.path.join(dest_folder, "xfwm4")))
+            version_folder = os.path.join(dest_folder, "xfwm4")
+            os.system("mkdir -p %s" % version_folder)
+            os.system("cp -R xfwm4-dark/*.png %s" % version_folder)
+            os.system("cp -R xfwm4-dark/themerc %s" % version_folder)
