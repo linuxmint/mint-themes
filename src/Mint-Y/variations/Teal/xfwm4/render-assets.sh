08bf237e0bc399787375b196ca9ec9e406d9f75d
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2025-07-12 15:43:06 +0200

    Xfwm4: Update variations

--- src/Mint-Y/variations/Teal/xfwm4/render-assets.sh
+++ src/Mint-Y/variations/Teal/xfwm4/render-assets.sh
@@ -26,4 +26,16 @@ if ! [ -f $ASSETS_DIR/$i.png ]; then
     $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png
 fi
 done
+
+if ! [ -f title-2-active.png ]; then
+  ln -s title-1-active.png title-2-active.png
+  ln -s title-1-active.png title-3-active.png
+  ln -s title-1-active.png title-4-active.png
+  ln -s title-1-active.png title-5-active.png
+  ln -s title-1-inactive.png title-2-inactive.png
+  ln -s title-1-inactive.png title-3-inactive.png
+  ln -s title-1-inactive.png title-4-inactive.png
+  ln -s title-1-inactive.png title-5-inactive.png
+fi
+
 exit 0
